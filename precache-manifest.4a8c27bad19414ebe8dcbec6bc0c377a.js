b515cfebc736eeaa16bd238f3eeebed7288138cc
FernandoNv <fernandonvieira47@hotmail.com>
2020-09-02 21:41:43 -0300

    Updates

---
+++ precache-manifest.4a8c27bad19414ebe8dcbec6bc0c377a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572324ee6f35c3962c4da649de7c8371",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "d9ba96b9d8b4cbbf93d8",
+    "url": "/drum-machine/static/css/main.a64b587d.chunk.css"
+  },
+  {
+    "revision": "5de4457c43a560e65d84",
+    "url": "/drum-machine/static/js/2.ebe64ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.ebe64ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ba96b9d8b4cbbf93d8",
+    "url": "/drum-machine/static/js/main.0c21f944.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  },
+  {
+    "revision": "8973faac1794830d15c459e4106ef776",
+    "url": "/drum-machine/static/media/pexels-wendy-wei.8973faac.jpg"
+  }
+]);
